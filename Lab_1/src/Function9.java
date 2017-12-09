a8242ea607b88b487f95a69603fcb65fc28066fa
TazievaIrina <i.tazieva@mail.ru>
2017-12-09 11:39:30 +0300

    Delete Function9.java

--- Lab_1/src/Function9.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Irishka on 08.10.2017.
- */
-public class Function9 implements Functions {
-    public double f ( double x){
-        return Math.pow(x-4,3)-Math.pow(x-1,2);
-    }
-}
