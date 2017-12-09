baea285abb6fae2e12624d8bdb7d0a0617c9ccf1
TazievaIrina <i.tazieva@mail.ru>
2017-12-09 11:36:11 +0300

    Delete Function10.java

--- Lab_1/src/Function10.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Irishka on 08.10.2017.
- */
-public class Function10 implements Functions {
-
-    public double f ( double x){
-        return Math.pow(x,3)-Math.pow(x-1,5);
-    }
-}
